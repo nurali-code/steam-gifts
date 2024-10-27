bd28dd686bcca65a0805c67bfaee6d1a67a6dc8b
nurali-code <nur3.dav.97@gmail.com>
2024-10-27 23:34:29 +0500

    ver 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,74 @@
 $('.btn__menu, .nav__link').on('click', function () {
-	$('.btn__menu, .nav, body').toggleClass('is_active')
+	window.innerWidth <= 1200 ? $('.btn__menu, .nav, body').toggleClass('is_active') : false;
 })
+
+$('.ddown_btn').on('click', function (e) {
+	e.stopPropagation();
+	$(this).closest('.ddown').toggleClass('is_active').find('.ddown-content').slideToggle(100);
+});
+
+$('.ddown_item').on('click', function () {
+	const $dropdown = $(this).closest('.ddown').removeClass('is_active');
+	$dropdown.find('.ddown_btn').html($(this).clone().find('input').remove().end().html());
+	$dropdown.find('.ddown-content').slideUp(200);
+});
+
+$(document).on('click', function (e) {
+	if (!$(e.target).closest('.ddown').length) {
+		$('.ddown.is_active').removeClass('is_active').find('.ddown-content').slideUp(100);
+	}
+});
+
+$('.service-slider').slick({
+	dots: false,
+	arrows: false,
+	infinite: false,
+	swipeToSlide: true,
+	speed: 300,
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	centerMode: false,
+	variableWidth: true,
+	responsive: [
+		{
+			breakpoint: 1500,
+			settings: { slidesToShow: 4, }
+		},
+		{
+			breakpoint: 1200,
+			settings: { slidesToShow: 2, }
+		},
+		{
+			breakpoint: 720,
+			settings: { slidesToShow: 1, }
+		},
+	]
+});
+
+$('.items-slider').slick({
+	dots: false,
+	arrows: false,
+	infinite: false,
+	swipeToSlide: true,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	centerMode: false,
+	variableWidth: true,
+	responsive: [
+		{
+			breakpoint: 1500,
+			settings: {
+				slidesToShow: 3,
+				// slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 2,
+				// slidesToScroll: 1,
+			}
+		},
+	]
+});
